0dfbbabd7ef4fd79abbd928ce9a8f2f6d1c47417
ad1tyq <adityabhardwaj9b7@gmail.com>
2026-06-17 11:51:40 +0530

    svg added

--- generate_images.py
+++ generate_images.py
@@ -25,11 +25,11 @@ async def main():
             
         progress = ""
         lang_list = ""
-        for name, data in sorted_langs[:5]:
+        for name, data in sorted_langs[:10]:
             color = data.get("color") or "#ccc"
             prop = data.get("prop", 0)
             progress += f'<span class="progress-item" style="background-color: {color}; width: {prop}%;" />\n'
-            lang_list += f'<li><svg class="octicon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg><span class="lang">{name}</span><span class="percent">{prop:.1f}%</span></li>\n'
+            lang_list += f'<li><svg class="octicon" style="fill: {color};" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg><span class="lang">{name}</span><span class="percent">{prop:.1f}%</span></li>\n'
             
         if template:
             result = template.replace("{{ progress }}", progress).replace("{{ lang_list }}", lang_list)
